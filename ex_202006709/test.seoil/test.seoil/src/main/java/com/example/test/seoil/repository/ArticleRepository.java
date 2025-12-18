546e2ba34ab87aea1a85397dea250f00035cc1ad
meokbo00 <158129888+meokbo00@users.noreply.github.com>
2025-12-18 13:59:58 +0900

    123

--- ex_202006709/test.seoil/test.seoil/src/main/java/com/example/test/seoil/repository/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.test.seoil.repository;
-
-import com.example.test.seoil.controller.Article;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ArticleRepository extends CrudRepository<Article, Long> {
-}
